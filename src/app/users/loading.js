e18adf6e499961a3e4f0a69b34c93be5ea11205c
prijitha unnikrishnan <drashtimedia98@gmail.com>
2023-10-12 23:56:56 +0530

    Loader in  Next.js

---
+++ src/app/users/loading.js
@@ -0,0 +1,9 @@
+import './userlist.css'
+
+export default function Page(){
+    return(
+        <div className="loader-line">
+            {/* <h1>Loading User List Data...</h1> */}
+        </div>
+    )
+}
---
+++ src/app/users/page.js
@@ -0,0 +1,45 @@
+async function userList() {
+    let data = await fetch("https://dummyjson.com/users");
+    data = await data.json();
+    return data.users;
+}
+export default async function Page() {
+
+    let users = await userList();
+    console.log(users);
+    return (
+        <div>
+            <h1>User Name List</h1>
+            {/* <table className='border'>
+                <thead>
+                    <tr>
+                        <th> First Name</th>
+                        <th>Last Name</th>
+                        <th>Age (in years)</th>
+                        <th>Gender</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user,index) => (
+                            <tr key={index}>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.age}</td>
+                                <td>{user.gender}</td>
+                                <td>{user.email}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+
+            </table> */}
+            {
+                users.map((user, index) => (
+                    <div><h2>User Name : {user.firstName}</h2></div>
+                ))}
+
+        </div>
+    )
+}
